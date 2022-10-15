c89d12968d82021de7ba95be6e6d9497cdd588b2
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-15 16:35:24 +0900

    1. Javaのコードを呼び出す

---
+++ src/main/java/JavaMain.java
@@ -0,0 +1,7 @@
+public class JavaMain {
+    public static void main(String[] args) {
+        // インスタンスを生成して使用する感覚で Kotlin のコードを呼び出せる
+        HelloByKotlin helloByKotlin = new HelloByKotlin();
+        helloByKotlin.printHello();
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    // インスタンスを生成して使用する感覚で Java のコードを呼び出せる
+    val hello = HelloByJava()
+    hello.printHello()
 }

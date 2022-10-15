dd34acd858c4fc6ec36c1c07fa1ee2077afd1b58
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-10-15 16:40:13 +0900

    2. Javaのライブラリを呼び出す

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
+import java.time.LocalDateTime
+import java.util.*
+
+data class Time(val time: LocalDateTime)
 fun main() {
     // インスタンスを生成して使用する感覚で Java のコードを呼び出せる
     val hello = HelloByJava()
     hello.printHello()
+
+    // Java のライブラリを呼び出すことも問題なくできる
+    val uuid: UUID = UUID.randomUUID()
+    println(uuid.toString())
+
+    // Java の標準ライブラリのクラスを型として使用することもできる
+    val now = Time(LocalDateTime.now())
+    println(now)
 }
